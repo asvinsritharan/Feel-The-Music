c6a835e6280df9868cb99f02bb7949b5bf86d920
asvinsritharan <asvinsritharan@hotmail.com>
2025-11-03 18:05:36 -0500

    fix: add embeddings to recommend_songs function input and remove hard links to dataframe and embeddings in run_recommender

--- src/run_recommender.py
+++ src/run_recommender.py
@@ -3,13 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import pandas as pd
 
-import sys
-sys.path.append('..')
 
-df = pd.read_csv("../data/in/songs_roberta.csv")
-embeddings = np.load("../data/in/lyrics_embeddings_roberta.npy")
-
-def recommend_songs(df, user_query, top_k=10):
+def recommend_songs(df, embeddings, user_query, top_k=10):
     model = SentenceTransformer("sentence-transformers/all-roberta-large-v1")
     query_vec = model.encode([user_query], normalize_embeddings=True)
     sims = cosine_similarity(embeddings, query_vec).flatten()
